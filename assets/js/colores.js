5fe1f7da061ac251ff7496630056ce3f7695a3a8
Roberto Fernández <soporte@tecnobooking.com>
2024-09-13 22:01:48 -0300

    Limpioo

--- assets/js/colores.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#colorBtn").addEventListener("click", () => {
-  if (document.querySelector(".color-box").style.backgroundColor === "red") {
-    document.querySelector(".color-box").style.backgroundColor = "lightblue";
-  } else {
-    document.querySelector(".color-box").style.backgroundColor = "red";
-  }
-});
